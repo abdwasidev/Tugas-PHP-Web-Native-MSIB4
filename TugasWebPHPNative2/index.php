43f03b2bc9d646300bc849bc1148ee456a6f88d8
Abdul Wasi <abdul.wasi.al.afif@gmail.com>
2023-05-07 05:46:49 +0700

    Add Route in Main Index Tugas WebPHPNative 2

--- TugasWebPHPNative2/index.php
+++ TugasWebPHPNative2/index.php
@@ -25,38 +25,27 @@
           <div class="data-hasil">
             <div class="scroll">
               <table width=100%>
-                <thead>
-                  <tr>
-                    <th>Dashboard</th>
-                    <th>Authentication</th>
-                    <th>Error</th>
-                  </tr>
-                </thead>
                 <tbody>
                   <tr>
-                    <td><a href="admin/" target="_blank" rel="noopener noreferrer">Home Dashboard</a></td>
-                    <td><a href="auth/" target="_blank" rel="noopener noreferrer">Home Auth</a></td>
-                    <td><a href="admin/error/" target="_blank" rel="noopener noreferrer">Home Error</a></td>
+                    <td><a href="admin/connection.php" target="_blank" rel="noopener noreferrer">Check Connection</a></td>
                   </tr>
                   <tr>
                     <td><a href="admin/?url=product" target="_blank" rel="noopener noreferrer">Data Produk</a></td>
-                    <td><a href="auth/?url=login" target="_blank" rel="noopener noreferrer">Login</a></td>
-                    <td><a href="admin/error/" target="_blank" rel="noopener noreferrer">Error 401</a></td>
                   </tr>
                   <tr>
-                    <td><a href="admin/?url=charts" target="_blank" rel="noopener noreferrer">Charts</a></td>
-                    <td><a href="auth/?url=register" target="_blank" rel="noopener noreferrer">Register</a></td>
-                    <td><a href="admin/error/?url=" target="_blank" rel="noopener noreferrer">Error 404</a></td>
+                    <td><a href="admin/?url=jenis_produk" target="_blank" rel="noopener noreferrer">Data Jenis Produk</a></td>
                   </tr>
                   <tr>
-                    <td><a href="admin/?url=tables" target="_blank" rel="noopener noreferrer">Tables</a></td>
-                    <td><a href="auth/?url=password" target="_blank" rel="noopener noreferrer">Forget Password</a></td>
-                    <td><a href="admin/error/?url=" target="_blank" rel="noopener noreferrer">Error 500</a></td>
+                    <td><a href="admin/?url=pelanggan" target="_blank" rel="noopener noreferrer">Data Pelanggan</a></td>
                   </tr>
                   <tr>
-                    <td><a href="admin/connection.php" target="_blank" rel="noopener noreferrer">Check Connection</a></td>
-                    <td><a href="#" target="_blank" rel="noopener noreferrer">-</a></td>
-                    <td><a href="#" target="_blank" rel="noopener noreferrer">-</a></td>
+                    <td><a href="admin/?url=pesanan" target="_blank" rel="noopener noreferrer">Data Pesanan</a></td>
+                  </tr>
+                  <tr>
+                    <td><a href="admin/?url=pembayaran" target="_blank" rel="noopener noreferrer">Data Pembayaran</a></td>
+                  </tr>
+                  <tr>
+                    <td><a href="admin/?url=kartu" target="_blank" rel="noopener noreferrer">Data Kartu</a></td>
                   </tr>
                 </tbody>
               </table>
